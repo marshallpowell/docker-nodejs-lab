b2d683683f931da25b3b076e0d1cdcc246287f54
marshallpowell <marshallpowell@gmail.com>
2015-08-23 22:12:59 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # docker-nodejs-lab
+
+## Prerequisites for this lab include
+
+### Download and Install Docker
+
+* Fedora / RedHat Users run the below commands:
+* `sudo wget -qO- https://get.docker.com/ | sh`
+* `sudo service docker start`
+
+* Other Linux Users: [click here](http://docs.docker.com/linux/step_one/)
+* Windows Users: [click here](http://docs.docker.com/windows/step_one/)
+* Mac Users: [click here](http://docs.docker.com/mac/step_one/) 
+
+
+### Clone this project 
+Navigate to the directory you will use for your project and run the below commands. If you are using the RH vagrant box, navigate to the /vagrant directory.
+
+* `git init`
+* `git remote add origin https://github.com/marshallpowell/docker-nodejs-lab.git`
+
+### Build the Docker image
+
+* Navigate to the docker-lab-client directory (where the DockerFile is)
+* Run this command to build the container: `sudo docker build -t vagrant/nodejs-client .`
+* Run this command to run the container: `sudo docker run -d -p 8000:8000 --name client vagrant/nodejs-client`
+* Open your browser and navigate to [http://localhost:8000](http://localhost:8000) , It should show a welcome page.
+* That's it.
+  
+
+
---
+++ docker-lab-client/nodejs/src/app.js
@@ -0,0 +1,110 @@
+var express = require('express');
+var expressHbs = require('express3-handlebars');
+var http = require('http');
+
+// Constants
+var PORT = 8000;
+
+// Initialize Express
+var app = express();
+app.engine('hbs', expressHbs({extname:'hbs', defaultLayout:'main.hbs'}));
+app.set('view engine', 'hbs');
+
+/*
+#These are example environment variables set by Docker at run time
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+HOSTNAME=fb70526e3fde
+MYSERVER_PORT=tcp://172.17.0.10:8001
+MYSERVER_PORT_8001_TCP=tcp://172.17.0.10:8001
+MYSERVER_PORT_8001_TCP_ADDR=172.17.0.10
+MYSERVER_PORT_8001_TCP_PORT=8001
+MYSERVER_PORT_8001_TCP_PROTO=tcp
+MYSERVER_NAME=/client/myserver
+*/
+
+var requestOptions = {
+    host : 'myserver', //this name is set in your /etc/host file for the docker service you are linking to
+    port : process.env.MYSERVER_PORT_8001_TCP_PORT,
+    path : '/', // url with parameters
+    method : 'GET' // GET Method
+};
+
+
+//Routes
+app.get("/helloWorld/", function (req, res) {
+
+        // HTTP GET request
+	console.log('Request made to /helloWorld/');
+
+	res.send('Hello World');
+
+});
+
+//Routes
+app.get("/", function (req, res) {
+
+        // HTTP GET request
+        console.log('Request made to /');
+
+  var basketballPlayers = [
+    {name: 'Lebron James', team: 'the Heat'},
+    {name: 'Kevin Durant', team: 'the Thunder'},
+    {name: 'Kobe Jordan',  team: 'the Lakers'}
+  ];
+  
+  var days = [
+    'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'
+  ];
+  
+  var person = {
+    name: 'Gorilla',
+    address: {
+      streetName: 'Broadway',
+      streetNumber: '721',
+      floor: 4,
+      addressType: {
+        typeName: 'residential'
+      }
+    }
+  };
+
+  var logic = {
+    upIsUp: true,
+    downIsUp: false,
+    skyIsBlue: "yes"
+  };
+
+  var data = {
+    basketballPlayers: basketballPlayers,
+    days: days,
+    person: person,
+    logic: logic
+  };
+
+
+        res.render('templateSamples', data);
+
+});
+
+app.get("/service/", function(req, res){
+
+	res.writeHead(200, {'Content-Type': 'application/json'});
+
+        var reqGet = http.request(requestOptions, function(response) {
+            response.on('data', function(data) { 
+		console.log(data);
+                  res.end(data); 
+            });
+        });
+
+        reqGet.end();
+        reqGet.on('error', function(e) {
+            console.error(e);
+        });
+
+});
+
+
+
+app.listen(PORT);
+console.log('Running on  33o http://localhost:' + PORT);
